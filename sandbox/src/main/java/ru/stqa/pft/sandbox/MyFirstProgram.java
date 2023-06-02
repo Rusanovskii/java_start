dc09d4ec46177882011a351a555e9614ee1bde9a
Rusanovskii <135255747+Rusanovskii@users.noreply.github.com>
2023-06-02 15:56:37 +0300

    Update MyFirstProgram.java

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,11 +1,11 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProgram{
+public class MyFirstProgram {
 
- public static void main(String[] args) {
+    public static void main(String[] args) {
 
-  System.out.println("Hello, world!");
- }
+        System.out.println("Hello, world!");
+    }
 
 }
 
