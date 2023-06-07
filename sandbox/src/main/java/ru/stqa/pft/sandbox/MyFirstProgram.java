ebd8358296b76b67147c3a0f43391a05cb59234c
Rusanovskii <135255747+Rusanovskii@users.noreply.github.com>
2023-06-07 13:26:34 +0300

    Изучил работу с функциями, добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,27 +3,28 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 //Класс был помещен в пакет, нужно ли прописывать полностью имя с указанием пакета в build.gradle?
     public static void main(String[] args) {
-// Используем переменную и для вывода простого слова, на случай его замены
-        String any = "World";
-        System.out.println("Hello, "  + any + "!");
-// Изучил применение стандартных матетематических операций
-        System.out.println(2 + 2);
-        System.out.println(2 - 2);
-        System.out.println(2*2);
-        System.out.println(2 / 2);
-        System.out.println(2.0 / 1);
-        System.out.println(1.0/2);
-// Изучил метод конкатенации
-        System.out.println("2" + "2");
-        System.out.println("2" + 2);
-        System.out.println(2 + 2 * 2);
-        System.out.println((2 + 2) * 2);
-        System.out.println("2 + 2 = " + (2 + 2));
-// Изучил использование переменной
-        double l = 8.0;
-        double s = l * l;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+        // Передаем значение для переменной any указывая его в парпметре функции
+        hello("World");
+        hello("Mr Barantsev");
+        hello("Java");
 
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+    }
+    public static void hello(String any) {
+        // Указываем, в параметре используемой в main функции, что any это строка
+        System.out.println("Hello, "  + any + "!");
+    }
+
+    public static double area (double v) {
+        return v * v;
+    }
+    public static double area (double a, double b) {
+        return a * b;
     }
 }
 
